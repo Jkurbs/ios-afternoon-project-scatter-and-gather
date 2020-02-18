1484b21f2dc40f8d2ddcbec5648e5112799a8682
Kerby Jean <kurbs@Kerbys-MacBook-Pro.local>
2020-02-18 14:54:19 -0500

    Setup storyboard

--- Scatter/Scatter/ViewController.swift
+++ Scatter/Scatter/ViewController.swift
@@ -10,11 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var isScattered: Bool = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func toggleButtonPressed_(_ sender: Any) {
+        if isScattered {
+            
+        } else {
+            
+        }
+    }
 }
 
